2f55c427190779e24c243e5534c870e50a78d56d
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-18 11:36:20 -0800

    writing file

--- app.js
+++ app.js
@@ -2,20 +2,20 @@
 const fs = require('fs')
 
 // fs.readFile is for reading documents
-fs.readFile('movie.txt', 'utf8', (err, data) => {
-    if (err) { console.log(err) }
-    console.log(data)
-})
+// fs.readFile('movie.txt', 'utf8', (err, data) => {
+//     if (err) { console.log(err) }
+//     console.log(data)
+// })
 
-// fs.writeFile
-fs.writeFile('pokemon.txt', 'This is a text file that you can keep track of all the pokemon in the world!', (err) => {
+// fs.writeFile is for creating a document
+fs.writeFile('pokemon.txt', 'This is a text file that you can keep track of all the pokemon in the world! STARTS HERE =====> ', (err) => {
     if (err) { console.log(err) }
     console.log('File Created!')
 })
 
-// fs.appendFile
-fs.appendFile('pokemon.txt', 'MewTwo', (err) =>{
-    if (err) { console.log(err) }
-    console.log('Pokemon File Updated')
-})
+// fs.appendFile is for adding to a document
+// fs.appendFile('pokemon.txt', 'MewTwo', (err) =>{
+//     if (err) { console.log(err) }
+//     console.log('Pokemon File Updated')
+// })
 
