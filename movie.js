0f9661d0ab236c071dbe2aee84e595657d6260ae
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-18 11:26:42 -0800

    tested writeFile for movie.js

--- movie.js
+++ movie.js
@@ -1,2 +1,7 @@
 // File System Module - require 'fs'
 const fs = require('fs')
+
+fs.writeFile('movie.txt', 'TheMask', (err) => {
+    if (err) { console.log(err) }
+    console.log('file created')
+})
