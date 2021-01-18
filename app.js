3354602569dbbddfc3ee21afcf011437a52d5999
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-18 11:55:02 -0800

    gif for read/write/append - status 200

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const fs = require('fs')
 
 // fs.readFile is for reading documents
-// fs.readFile('pokemon.txt', 'utf8', (err, data) => {
-//     if (err) { console.log(err) }
-//     console.log(data)
-// })
+fs.readFile('pokemon.txt', 'utf8', (err, data) => {
+    if (err) { console.log(err) }
+    console.log(data)
+})
 
 // fs.writeFile is for creating a document
 fs.writeFile('pokemon.txt', 'This is a text file that you can keep track of all the pokemon in the world! STARTS HERE =====> ', (err) => {
@@ -14,8 +14,8 @@ fs.writeFile('pokemon.txt', 'This is a text file that you can keep track of all 
 })
 
 // fs.appendFile is for adding to a document
-// fs.appendFile('pokemon.txt', 'MewTwo, Charizard, Blastoise, Bulbasaur', (err) =>{
-//     if (err) { console.log(err) }
-//     console.log('Pokemon File Updated')
-// })
+fs.appendFile('pokemon.txt', 'MewTwo, Charizard, Blastoise, Bulbasaur', (err) =>{
+    if (err) { console.log(err) }
+    console.log('Pokemon File Updated')
+})
 
